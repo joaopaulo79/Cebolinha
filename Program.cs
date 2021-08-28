69193b3e70dff5975fc5915ba34ec750bd251949
João Paulo <joaopdejesus79@gmail.com>
2021-08-28 20:24:12 -0300

    Programa Finalizado!

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Texto;
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Digite o seu texto: ");
+            Console.ResetColor();
+            Texto = Console.ReadLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write($"Texto com dilalia: ");
+            Console.ResetColor();
+            Console.WriteLine(Texto
+                            .Replace("r","l")
+                            .Replace("R","L"));
+            Console.WriteLine();
+
+        }
+    }
+}
